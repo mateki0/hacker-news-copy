235893c58168b426594807c36b042fffbcae4029
mateki0 <mateki0@interia.pl>
2020-11-07 06:42:02 +0100

    search box height lowered

--- src/Components/SearchBar/styled/SearchBox.js
+++ src/Components/SearchBar/styled/SearchBox.js
@@ -5,7 +5,7 @@ const SearchBox = styled.input`
   box-shadow: 0px 0px 6px 0px rgba(0,0,0,0.1);
   border-radius:8px;
   width:93%;
-  font-size:22px;
+  font-size:18px;
   font-family: 'Pathway Gothic One', sans-serif;
   padding-left:15px;
   @media only screen and(min-width:768px){
--- src/Components/SearchBar/styled/SearchWrapper.js
+++ src/Components/SearchBar/styled/SearchWrapper.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const SearchWrapper = styled.div`
   max-width:500px;
   width:100%;
-  height:60px;
+  height:50px;
   position:relative;
   display:grid;
   margin:0 auto 20px auto;
